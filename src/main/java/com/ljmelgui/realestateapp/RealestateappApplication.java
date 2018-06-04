435a3095eae69b6bcf267c2604c3859fd2cb6b5d
loren <ljmelgui@gmail.com>
2018-06-04 13:02:11 +0200

    Milestone 1: Recently created project

---
+++ src/main/java/com/ljmelgui/realestateapp/RealestateappApplication.java
@@ -0,0 +1,12 @@
+package com.ljmelgui.realestateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealestateappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealestateappApplication.class, args);
+	}
+}

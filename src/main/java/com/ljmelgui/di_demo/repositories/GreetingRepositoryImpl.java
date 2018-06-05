a1142fa44f3ce8cf5bfc7a5e934f7bb90aad99af
loren <ljmelgui@gmail.com>
2018-06-05 19:22:35 +0200

    BUG

--- src/main/java/com/ljmelgui/di_demo/repositories/GreetingRepositoryImpl.java
+++ src/main/java/com/ljmelgui/di_demo/repositories/GreetingRepositoryImpl.java
@@ -4,7 +4,7 @@ public class GreetingRepositoryImpl implements GreetingRepository {
 
     @Override
     public String getEnglishGreeting() {
-        return "Hallo vom Hauptdienst";
+        return "Hello - Primary Greeting service";
     }
 
     @Override
@@ -14,6 +14,6 @@ public class GreetingRepositoryImpl implements GreetingRepository {
 
     @Override
     public String getGermanGreeting() {
-        return "Hello - Primary Greeting service";
+        return "Hallo vom Hauptdienst";
     }
 }
